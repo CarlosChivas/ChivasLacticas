4e0b7e14491572e6911dd19ac5c792a0d9a1f698
NataliaVg <natalia.velasco.ga@gmail.com>
2021-03-25 14:29:48 -0600

    Código

---
+++ Lexer.cpp
@@ -0,0 +1,180 @@
+#include "Lexer.h"
+#include <iostream>
+
+using namespace std;
+
+vector<Token> Lexer::LexerAritmetico(string text)
+{
+    vector<Token> tokens;
+    Token cToken;
+
+    for (char cChar : text)
+    {
+        if(cToken.sType == Comentario){
+            cToken.sText.append(1, cChar);
+            continue;
+        }
+
+        switch (cChar)
+        {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                if (cToken.sType == Whitespace)
+                {
+                    cToken.sType = Entero;
+                    cToken.sText.append(1, cChar);
+                } else if (cToken.sType == Flotante_exponencial) {
+                    cToken.sType = Flotante_exponencial_positivo;
+                    cToken.sText.append(1, cChar);
+                } else if (cToken.sType == Division){
+                    addToken(cToken, tokens);
+                    cToken.sType = Entero;
+                    cToken.sText.append(1, cChar);
+                }
+                else {
+                    cToken.sText.append(1, cChar);
+                }
+            break;
+
+            case '.':
+                if(cToken.sType != Entero){
+                    addToken(cToken, tokens);
+                    cToken.sType = Error_token;
+                    cToken.sText.append(1, cChar);
+                    addToken(cToken, tokens);
+                }
+                else{
+                    if (cToken.sType == Entero)
+                    {
+                        cToken.sType = Flotante;
+                        cToken.sText.append(1, cChar);
+                    }
+                }
+                break;
+            case '(':
+                addToken(cToken, tokens);
+                cToken.sType = Parentesis_que_abre;
+                cToken.sText.append(1, cChar);
+                addToken(cToken, tokens);
+                    break;
+            case ')':
+                addToken(cToken, tokens);
+                cToken.sType = Parentesis_que_cierra;
+                cToken.sText.append(1, cChar);
+                addToken(cToken, tokens);
+                break;
+            case '=':
+                addToken(cToken, tokens);
+                cToken.sType = Asignacion;
+                cToken.sText.append(1, cChar);
+                addToken(cToken, tokens);
+                break;
+            case '+':
+                if(cToken.sType != Variable || cToken.sType != Entero){
+                    addToken(cToken, tokens);
+                    cToken.sType = Error_token;
+                    addToken(cToken, tokens);
+                }
+                else{
+                    addToken(cToken, tokens);
+                    cToken.sType = Suma;
+                    cToken.sText.append(1, cChar);
+                    addToken(cToken, tokens);
+                }
+                break;
+            case '-':
+                if (cToken.sType == Flotante_exponencial) {
+                    cToken.sType = Flotante_exponencial_negativo;
+                    cToken.sText.append(1, cChar);
+                }
+                else {
+                    addToken(cToken, tokens);
+                    cToken.sType = Resta;
+                    cToken.sText.append(1, cChar);
+                    addToken(cToken, tokens);
+                }
+                break;
+
+            case '^':
+                    addToken(cToken, tokens);
+                    cToken.sType = Potencia;
+                    cToken.sText.append(1, cChar);
+                    addToken(cToken, tokens);
+                break;
+
+            case '*':
+                    addToken(cToken, tokens);
+                    cToken.sType = Multiplicacion;
+                    cToken.sText.append(1, cChar);
+                    addToken(cToken, tokens);
+                break;
+
+            case '/':
+                if(cToken.sType != Division){
+                    addToken(cToken, tokens);
+                    cToken.sType = Division;
+                    cToken.sText.append(1, cChar);
+                }
+                else if(cToken.sType == Division){
+                    cToken.sType = Comentario;
+                    cToken.sText.append(1, cChar);
+                }
+                break;
+
+            case ' ':
+            case '\t':
+                addToken(cToken, tokens);
+                break;
+
+            case 'e':
+            case 'E':
+                if(cToken.sType == Flotante){
+                    cToken.sType = Flotante_exponencial;
+                    cToken.sText.append(1, cChar);
+                    break;
+                }
+
+            default:
+                if (!isdigit(cChar) &&  !isalpha(cChar) && cChar != '_') {
+                    addToken(cToken, tokens);
+                    cToken.sType = Error_token;
+                    cToken.sText.append(1, cChar);
+                    addToken(cToken, tokens);
+                } else {
+                    if(cToken.sType != Variable){
+                    addToken(cToken, tokens);
+                    cToken.sType = Variable;
+                    cToken.sText.append(1, cChar);
+                    }
+                    else{
+                        cToken.sText.append(1, cChar);
+                    }
+
+                }
+                break;
+        }
+    }
+    addToken(cToken, tokens);
+    return tokens;
+}
+
+void Lexer::addToken(Token &token, vector<Token> &tokens) {
+    if (token.sType != Whitespace) {
+        tokens.push_back(token);
+    }
+    token.sType = Whitespace;
+    token.sText.erase();
+}
+
+void Token::lPrint()
+{
+    cout << sText << "          " << TypeLArray[sType]<< endl;
+}
---
+++ Lexer.h
@@ -0,0 +1,61 @@
+#ifndef LEXER_H
+#define LEXER_H
+#include <vector>
+#include <string>
+using namespace std;
+
+enum TypeL {
+    Whitespace,
+    Entero,
+    Flotante,
+    Flotante_exponencial,
+    Flotante_exponencial_negativo,
+    Flotante_exponencial_positivo,
+    Asignacion,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Potencia,
+    Variable,
+    Parentesis_que_abre,
+    Parentesis_que_cierra,
+    Comentario,
+    Error_token
+};
+
+static const char *TypeLArray[] = {
+    "Whitespace",
+    "Entero",
+    "Flotante",
+    "Flotante_exponencial",
+    "Flotante_exponencial_negativo",
+    "Flotante_exponencial_positivo",
+    "Asignacion",
+    "Suma",
+    "Resta",
+    "Multiplicacion",
+    "Division",
+    "Potencia",
+    "Variable",
+    "Parentesis_que_abre",
+    "Parentesis_que_cierra",
+    "Comentario",
+    "Error_token"
+};
+
+class Token{
+ public:
+    enum TypeL sType{Whitespace};
+    string sText;
+    void lPrint();
+};
+
+class Lexer {
+    private:
+        void addToken(Token &token, vector<Token> &tokens);
+    public:
+        vector<Token> LexerAritmetico(string text);
+};
+
+#endif
